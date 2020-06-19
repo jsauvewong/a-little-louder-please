de6a65330d2924d9112100071e3e8ccb085f6e39
JS <jsauvewong@gmail.com>
2020-06-18 22:37:13 -0700

    fixing port

--- src/express.ts
+++ src/express.ts
@@ -3,11 +3,11 @@ import express from 'express'
 export const startServer = () => {
 
   const app = express()
-  const port = 5000
+  const onDone = () => console.log(`Example app listening at http://localhost:${5000}`)
 
   app.get('/', (req, res) => res.send('Hello World!'))
   app.get('/js', (req, res) => res.send('Hello Patooter!'))
 
-  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+  app.listen(process.env.PORT || 5000, onDone)
 
 }
