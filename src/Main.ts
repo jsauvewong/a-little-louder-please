1e390559b87b94e3666989f558503dd83a7119f2
JS <jsauvewong@gmail.com>
2020-07-03 18:39:13 -0700

    should send it out tomorrow at 9am

--- src/Main.ts
+++ src/Main.ts
@@ -2,22 +2,25 @@ import { sendSmsQuote } from './sendSms'
 import { pickPhrase } from './pickPhrase'
 import { startServer } from './express'
 import { setUpDatabase } from './postgreSQL'
+import { schedule } from 'node-schedule'
 
 // So the app can talk to Heroku on their own assigned port
 const letsRunThisBaby = async () => {
   startServer()
-  const execute = () => {
-    const randomPhrase = pickPhrase()
-    sendSmsQuote(randomPhrase)
-    console.log('sent')
-  }
-  //comment out until ready for it contionously send a message
-  // const j = schedule.scheduleJob('0 9 * * *', function () {
-  //   sendSms(pickPhrase())
-  //   console.log('done!')
-  // })
+
+  // const execute = () => {
+  //   const randomPhrase = pickPhrase()
+  //   sendSmsQuote(randomPhrase)
+  //   console.log('sent')
+  // }
+
+  // comment out until ready for it contionously send a message
+  const j = schedule.scheduleJob('0 9 * * *', function () {
+    sendSmsQuote(pickPhrase())
+    console.log('done!')
+  })
   await setUpDatabase()
-  execute()
+  // execute()
 }
 
 letsRunThisBaby()
